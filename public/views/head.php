bb066e89dfa3adae322d941cb0f6f151d3286b1a
murean <rezyanshari@gmail.com>
2017-07-11 21:15:54 +0700

    Integrate offline Google font in HTML head

--- public/views/head.php
+++ public/views/head.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html lang="en-US">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Minimalista - New Amazing HTML5 Template</title>
-    <link rel="stylesheet" href="/css/components.css">
-    <link rel="stylesheet" href="/css/icons.css">
-    <link rel="stylesheet" href="/css/responsee.css">
-    <link rel="stylesheet" href="/owl-carousel/owl.carousel.css">
-    <link rel="stylesheet" href="/owl-carousel/owl.theme.css">
-    <link rel="stylesheet" href="/css/lightcase.css">
-    <!-- CUSTOM STYLE -->
-    <link rel="stylesheet" href="/css/template-style.css">
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,400,600,900&subset=latin-ext" rel="stylesheet">
-    <script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>
-    <script type="text/javascript" src="/js/jquery-ui.min.js"></script>
-  </head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Minimalista - New Amazing HTML5 Template</title>
+        <link rel="stylesheet" href="/css/components.css">
+        <link rel="stylesheet" href="/css/icons.css">
+        <link rel="stylesheet" href="/css/responsee.css">
+        <link rel="stylesheet" href="/owl-carousel/owl.carousel.css">
+        <link rel="stylesheet" href="/owl-carousel/owl.theme.css">
+        <link rel="stylesheet" href="/css/lightcase.css">
+        <link rel="stylesheet" href="/css/font.css">
+        <!-- CUSTOM STYLE -->
+        <link rel="stylesheet" href="/css/template-style.css">
+        <!--        <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,400,600,900&subset=latin-ext" rel="stylesheet">-->
+        <script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="/js/jquery-ui.min.js"></script>
+    </head>
 
-  <body class="size-1140">
+    <body class="size-1140">
 
-    <div id="page-wrapper">
-      
+        <div id="page-wrapper">
+
