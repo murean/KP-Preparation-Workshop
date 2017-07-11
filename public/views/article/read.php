687e61423af00d322064e3e5e021e1bfa5f2a3b4
murean <rezyanshari@gmail.com>
2017-07-11 21:16:53 +0700

    Add Dynamic feature for Articles Views

--- public/views/article/read.php
+++ public/views/article/read.php
@@ -3,10 +3,19 @@
 
 <main role="main">
     <header class="section background-white">
-      <div class="line text-center">
-        <h1 class="text-dark text-s-size-30 text-m-size-40 text-l-size-headline text-thin text-line-height-1">Loriem Ipsuemes</h1>         
-      </div>
+        <div class="line text-center">
+            <h1 class="text-dark text-s-size-30 text-m-size-40 text-l-size-headline text-thin text-line-height-1">
+                <?php echo $article['title'] ?>
+            </h1>
+        </div>
     </header>
+    <img src="/img/attachment/img-<?php echo $article['id'] ?>.jpg" alt="" class="full-img">
+    <div class="section-small-padding full-width background-white">
+        <div class="m-12 s-12 l-7">
+            <?php echo $article['content'] ?>
+        </div>
+
+    </div>
 </main>
 
 <?php echo $foot_content ?>
